0fb8931bbd0d024dc8411b4da727f0d1cdc89279
Emiliano0X1 <egp2506@gmail.com>
2024-09-22 14:08:38 -0700

    PARO JUAN

---
+++ mindblind/src/app/components}/header.js
@@ -0,0 +1,16 @@
+
+export default function Home() {
+    return (
+      <div className="bg-white flex"> 
+        <div >
+
+        </div>
+        <div>
+            
+        </div>
+        <div>
+            
+        </div>
+      </div>
+    );
+  }
