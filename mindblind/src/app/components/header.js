491f49807f00d4769d46a9959e2e923334ed0d93
Dusuan <XxDusuanxX@hotmail.com>
2024-09-23 08:32:05 -0700

    rounding header

--- mindblind/src/app/components/header.js
+++ mindblind/src/app/components/header.js
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <>
-     <div className="flex">
-     <div style={{ background: "linear-gradient(to right, #1978f8, #0053f1)"}} className="h-full w-1"></div>
+     <div className="flex p-5 ">
+     <div style={{ background: "linear-gradient(to right, #1978f8, #0053f1)"}} className="h-full w-1 rounded-sm"></div>
       <div className="w-full">
-      <div style={{ background: "linear-gradient(#0053f1, #1978f8)"}} className="h-2"></div>
+      <div style={{ background: "linear-gradient(#0053f1, #1978f8)"}} className="h-2 rounded-sm"></div>
         <div
           style={{ backgroundColor: "#ece9d8" }}
           className="flex justify-between text-black py-10 px-2 text-sm"
@@ -19,9 +19,9 @@ export default function Header() {
             <p>.</p>
           </div>
         </div> 
-        <div style={{ background: "linear-gradient(#1978f8, #0053f1)"}} className="h-1"></div>
+        <div style={{ background: "linear-gradient(#1978f8, #0053f1)"}} className="h-1 rounded-sm"></div>
       </div>
-      <div style={{ background: "linear-gradient(to right, #0053f1, #1978f8)"}} className="h-full w-1"></div>
+      <div style={{ background: "linear-gradient(to right, #0053f1, #1978f8)"}} className="h-full w-1 rounded-sm"></div>
       
 {/* 99937c*/ }
     </div>
