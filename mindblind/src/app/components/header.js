0bc8e0f3306ed90171ce1c5838d24a96ec085e20
Emiliano0X1 <egp2506@gmail.com>
2024-09-22 19:10:25 -0700

    xd

--- mindblind/src/app/components/header.js
+++ mindblind/src/app/components/header.js
@@ -5,7 +5,6 @@ export default function Header() {
      <div style={{ background: "linear-gradient(to right, #1978f8, #0053f1)"}} className="h-full w-1"></div>
       <div className="w-full">
       <div style={{ background: "linear-gradient(#0053f1, #1978f8)"}} className="h-2"></div>
-
         <div
           style={{ backgroundColor: "#ece9d8" }}
           className="flex justify-between text-black py-10 px-2 text-sm"
@@ -21,12 +20,10 @@ export default function Header() {
           </div>
         </div> 
         <div style={{ background: "linear-gradient(#1978f8, #0053f1)"}} className="h-1"></div>
-
       </div>
       <div style={{ background: "linear-gradient(to right, #0053f1, #1978f8)"}} className="h-full w-1"></div>
       
 {/* 99937c*/ }
-
     </div>
     </>
   );
