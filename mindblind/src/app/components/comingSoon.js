6bac934675136c80c534098d89e56db8da092f61
Dusuan <XxDusuanxX@hotmail.com>
2024-09-25 11:11:18 -0700

    responsivness header omg

--- mindblind/src/app/components/comingSoon.js
+++ mindblind/src/app/components/comingSoon.js
@@ -1,23 +1,21 @@
 "use client";
 
-import {useState , useEffect} from "react";
-
+import { useState, useEffect } from "react";
 
 export default function Proximamente({ fonts }) {
+  const [cont, setCont] = useState(0);
 
-    const [cont , setCont] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCont((prevCont) => (prevCont + 1) % fonts.length);
+    }, 100);
+    console.log("Current font:", fonts[cont]);
+    return () => clearInterval(interval);
+  }, []);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCont((prevCont) => (prevCont + 1) % fonts.length);
-        }, 150);
-        console.log('Current font:', fonts[cont]);
-        return () => clearInterval(interval);
-    },[]);
-    
-    return (
-        <div className='flex justify-center items-center'>
-            <h1 className = {`font-${fonts[cont]}`}> COMING SOON </h1>
-        </div>
-    );
-};
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className={`font-${fonts[cont]}`}> COMING SOON </h1>
+    </div>
+  );
+}
--- mindblind/src/app/components/header.js
+++ mindblind/src/app/components/header.js
@@ -1,39 +1,70 @@
 import Proximamente from "./comingSoon";
 
 export default function Header() {
-  const fonts = ["windows_regular","disney_heroic","katakana","winbroken","dezire","franchise","hatolie","retroPower","shortPlan","korean"];
+  const fonts = [
+    "windows_regular",
+    "disney_heroic",
+    "katakana",
+    "winbroken",
+    "dezire",
+    "franchise",
+    "hatolie",
+    "retroPower",
+    "shortPlan",
+    "korean",
+  ];
   return (
-    
     <>
-    <div className="flex flex-col justify-between">
-     <div className="flex p-5  ">
-     <div style={{ background: "linear-gradient(to right, #1978f8, #0053f1)"}} className="h-full w-1 rounded-sm"></div>
-      <div className="w-full">
-      <div style={{ background: "linear-gradient(#0053f1, #1978f8)"}} className="h-2 rounded-sm"></div>
-        <div
-          style={{ backgroundColor: "#ece9d8" }}
-          className="flex justify-between text-black py-8 px-3 text-sm"
-        >
-          <div className="">
-            <p className=" text-xl md:text-2xl font-windows_regular">MINDBLIND STUDIOS  TM </p>
+      <div className="flex flex-col justify-between">
+        <div className="flex p-5  ">
+          <div
+            style={{
+              background: "linear-gradient(to right, #1978f8, #0053f1)",
+            }}
+            className="h-full w-1 rounded-sm"
+          ></div>
+          <div className="w-full">
+            <div
+              style={{ background: "linear-gradient(#0053f1, #1978f8)" }}
+              className="h-2 rounded-sm"
+            ></div>
+            <div
+              style={{ backgroundColor: "#ece9d8" }}
+              className="flex justify-between text-black py-8 px-3 text-sm"
+            >
+              <div className="">
+                <p className="hidden md:block text-sm md:text-2xl font-windows_regular">
+                  MINDBLIND STUDIOS TM{" "}
+                </p>
+              </div>
+              <div className="text-xl w-50 md:w-80 md:text-2xl align-middle">
+                <Proximamente fonts={fonts}></Proximamente>
+              </div>
+              <div>
+                <a
+                  href="https://www.instagram.com/mindblindstudios/"
+                  className="hidden md:block text-sm font-windows_regular md:text-xl md:hover:text-4xl transition-all"
+                >
+                  Siguenos en Instagram
+                </a>
+              </div>
+            </div>
+            <div
+              style={{ background: "linear-gradient(#1978f8, #0053f1)" }}
+              className="h-1 rounded-sm"
+            ></div>
           </div>
-          <div className="text-sm w-60 md:text-xl">
-          <Proximamente fonts={fonts}></Proximamente>
-          </div>
-          <div>
-            <a href="https://www.instagram.com/mindblindstudios/" className=" text-xl font-windows_regular md:text-xl md:hover:text-4xl transition-all">Siguenos en Instagram</a>
-          </div>
-        </div> 
-        <div style={{ background: "linear-gradient(#1978f8, #0053f1)"}} className="h-1 rounded-sm"></div>
+          <div
+            style={{
+              background: "linear-gradient(to right, #0053f1, #1978f8)",
+            }}
+            className="h-full w-1 rounded-sm"
+          ></div>
+
+          {/* 99937c*/}
+        </div>
+        <div></div>
       </div>
-      <div style={{ background: "linear-gradient(to right, #0053f1, #1978f8)"}} className="h-full w-1 rounded-sm"></div>
-      
-{/* 99937c*/ }
-    </div>
-    <div>
-      
-    </div>
-    </div>
     </>
   );
 }
