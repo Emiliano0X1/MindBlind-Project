95e6b4b83ada73d17098c6ffe78eba982acbd69f
Dusuan <143571151+Dusuan@users.noreply.github.com>
2024-10-10 05:01:22 +0000

    eliminated the header

--- mindblind/src/app/page.js
+++ mindblind/src/app/page.js
@@ -20,14 +20,14 @@ const fonts = [
 
 export default function Home() {
   return (
-    <div className="bg-cyan-500 bg-[url('/newBG.jpg')]  bg-cover flex flex-col w-screen h-screen md:justify-between">
-      <Header></Header>
-      <div className="flex flex-col items-center mt-12 md:mt-0">
+    <div className="bg-cyan-500 bg-[url('/newBG.jpg')]  bg-cover flex items-center justify-center flex-col w-screen h-screen w-screen min-h-screen">
+      <div>
+
+      </div>
+      <div className="flex flex-col items-center md:mt-0">
         <Logoframe images={images}></Logoframe>
-        <div className="flex justify-center items-center text-black p-10 lg:text-6xl md: text-xl">
-          
-        </div>
-      
+        {/* <div className="flex justify-center items-center text-black p-10 lg:text-6xl md: text-xl">
+        </div> */}
       </div>
       <div>
 
